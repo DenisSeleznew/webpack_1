0b810a1302a3ac3ed5c670bcd0920762351a81b3
Denis <generalpokrichkin@icloud.com>
2024-04-15 22:08:23 +0300

    installing plugins and building the project

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 import '/index.css';
-console.log('Hello World!');
+import JS_IMAGE from '/assets/JS-01_image.jpeg';
+
+const imgHTML = document.createElement('img');
+imgHTML.src = JS_IMAGE;
+document.body.append(imgHTML);
